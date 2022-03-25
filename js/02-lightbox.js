1786a90f958383f9bf0f56cf116d0a265208c3e2
dpartner <d.partner.ua@gmail.com>
2022-03-25 17:47:53 +0200

    2 task comleted

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,35 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+{
+  /* <div class="gallery__item">
+  <a class="gallery__item" href=${itemProp.original}>
+  <img class="gallery__image" src=${itemProp.preview} alt=${itemProp.description} />
+</a>
+</div>; */
+}
+
+const container = document.querySelector('.gallery');
+
+const createGalleryItemMarkup = itemProp => {
+  return `<div class="gallery__item">
+  <a class="gallery__item" href=${itemProp.original}>
+  <img class="gallery__image" src=${itemProp.preview} alt=${itemProp.description} />
+</a>
+</div>`;
+};
+
+const allGaleryItems = galleryItems.map(createGalleryItemMarkup).join('');
+
+container.insertAdjacentHTML('afterbegin', allGaleryItems);
+
+container.addEventListener('click', onOpenModal);
+
+function onOpenModal(event) {
+  event.preventDefault();
+  const lightBox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
